c775f2eeaccd8eeee8ff9fe6f6a61d65898593f4
minhajm12 <minhajatholi222@gmail.com>
2024-01-12 18:11:32 +0530

    navbar set

---
+++ assets/js/main.js
@@ -0,0 +1,86 @@
+// Open Menu
+let dropnav_open = document.querySelectorAll('.offcnvs-nav');
+dropnav_open.forEach(function (dropnav_opens) {
+    dropnav_opens.addEventListener('click', function () {
+        document.querySelector(".nav-canvas-items").classList.add('canvas-active');
+      document.querySelector(".canvas-main").classList.add('cnvsmain-active');
+    //   document.querySelector(".industry").classList.add('industry-active');
+    })
+      
+})
+
+// Close Menu
+let dropnav_close = document.querySelector('.canvas-main')
+dropnav_close.addEventListener('click', function () {
+      document.querySelector(".nav-canvas-items").classList.remove('canvas-active');
+      document.querySelector(".canvas-main").classList.remove('cnvsmain-active');
+      document.querySelectorAll(".nav-items-tabs").classList.add('nav-items-tabs-dnone');
+      
+})
+
+
+
+
+
+
+
+
+// //Click Outside Display None
+// document.body.addEventListener('click', event => {
+//     // check if the clicked element is the 'my-element' element or one of its children
+//     const clickedElement = event.target;
+//     const myElement = document.querySelector('.menuopen');
+//     if (myElement.contains(clickedElement)) {
+//       // do nothing if the clicked element is the 'my-element' element or one of its children
+//       return;
+//     }
+
+//     // remove the 'active' class from the 'my-element' element
+//     document.querySelector('.menucnvs').classList.remove('menucnvs-active')
+//   });
+
+
+
+
+
+
+
+
+// CUSTM NAV TABS
+
+// let industrylink = document.querySelector('.industry');
+// industrylink.addEventListener('click', function () {
+//     document.querySelector(".industrysection").classList.toggle('d-none');
+// })
+
+// let commitmentlink = document.querySelector('.commitment');
+// commitmentlink.addEventListener('click', function () {
+//     document.querySelector(".commitmentsection").classList.toggle('d-none');
+// })
+
+// let companylink = document.querySelector('.company');
+// companylink.addEventListener('click', function () {
+//     document.querySelector(".companysection").classList.toggle('d-none');
+    
+// })
+
+
+
+
+
+
+
+// NAVLINK tabs
+
+document.querySelectorAll('.offcnvs-nav').forEach(function(link) {
+    link.addEventListener('click', function () {
+        document.querySelectorAll('.nav-items-tabs').forEach(function(section) {
+            section.classList.add('d-none');
+        });
+
+        let targetSection = document.querySelector(`.${this.dataset.target}`);
+        if (targetSection) {
+            targetSection.classList.remove('d-none');
+        }
+    });
+});
