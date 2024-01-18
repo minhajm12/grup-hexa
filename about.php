14b9a8271a17f4cb272100af93b3273f250942ef
minhajm12 <minhajatholi222@gmail.com>
2024-01-18 18:54:17 +0530

    changes

---
+++ about.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hexa-About</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="assets/img/logo/favicon.png" type="image/x-icon">
+
+
+    <!-- Bootstrap CDN CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- owl carousel -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
+
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+</head>
+
+<body>
+    <!-- About Hero Section -->
+    <div id="about-hero">
+
+
+    <?php include('inc/header.php');?>
+
+        <div class="inner-pg-bnner">
+            <div class="container">
+                <div class="inner-main-title">
+                    <h2>Hexa Outlook</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <!-- aboutsection -->
+
+    <div id="abt-page-sec">
+        <div class="container">
+            <div class="top-sec">
+                <div class="row">
+                    <div class="col-12 col-lg-6 left">
+                        <div>
+                            <p class="top-title text-start">About Us</p>
+                            <h2 class="main-title">Outlook & <span class="main-grade"><br class="d-block d-md-none">Strategic Vision</span></h2>
+                        </div>
+                        <div>
+                            <p class="main-para text-start">Within 2021-2031, Grup Hexa will push ahead with its strategy to develop its businesses, principally in nurturing its domestic markets by investing in O&G Services, Engineering & Construction, Chemical Manufacturing & Distribution, Information Technology, Business Engineering, Media, etc.</p>
+                            <p class="main-para text-start">In Concessions, development will be principally focused on EPC in the infrastructural development in both industrial sector and commercial sector as the Group continues to diversify its concession portfolio and extend its maturity. This development dynamic will also apply to the Contracting business lines such as marine and composite engineering. In both businesses, growth will be boosted by innovation, where advances in technology and services will trigger the creation of new solutions, helping to refresh their business models.</p>
+                        </div>
+                        <!-- <a href="" class="main-btn">Know More</a> -->
+                    </div>
+                    <div class="col-12 col-lg-6 right">
+                        <div class="main-right">
+                            <div class="img-div">
+                                <img src="assets/img/abt-page-img-1.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="counter-sec">
+                                <div class="counter-div-main">
+                                    <h3 class="counter-div"><span class="counter">3</span>+</h3>
+                                    <p>Years of<br>Experience</p>
+                                </div>
+                                <div class="counter-div-main">
+                                    <h3 class="counter-div"><span class="counter">7</span>+</h3>
+                                    <p>Ventures</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bottom-sec">
+                <div class="row">
+                    <div class="col-12 col-lg-6 left order-2 order-lg-1">
+                        <div class="img-div">
+                            <img src="assets/img/abt-page-img-2.png" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 right order-1 order-lg-2">
+                        <p class="main-para text-start">The Group’s long term prospects also involve transforming its business lines, products and services, to provide sustainable solutions for the major challenges arising in today’s world.</p>
+                        <p class="main-para text-start">The Group’s activities are a driving force in the transformation of cities and regions. Guided by its long-term perspective and vision of all-round performance, Grup Hexa aims to make a positive contribution to that transformation.</p>
+                        <p class="main-para text-start">The Group has embarked on an in-depth conversation, with many of its teams and all its stakeholders, about the meaning of its mission, to home in on its ultimate purpose. We will share that purpose with everyone in our business ecosystem within 2021–2031.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+    <!-- CTA SECTION -->
+
+    <div class="cta-sec-main">
+        <div id="ctasection">
+            <div class="container">
+                <div class="back-logo">
+                    <img src="assets/img/logo/cta-logo.png" alt="">
+                </div>
+                <div class="cta-main">
+                    <h3 class="main-title">Connect with <span class="main-grade">Grup Hexa</span></h3>
+                    <p class="main-para">Gruphexa transforms communities, designing, building, and operating infrastructure
+                        to enhance daily life and mobility. As investors and operators, we play a vital role in shaping
+                        cities and regions.</p>
+                    <a href="" class="main-btn">Contact Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+   <!-- Service Section -->
+   <div id="servicesection">
+    <div class="container">
+        <div class="top-sec">
+            <p class="top-title">Services</p>
+            <h2 class="main-title">Grup Hexa - <span class="main-grade">Verticals</span></h2>
+        </div>
+        <div class="services">
+            <div class="service-item">
+                <div class="img-div">
+                    <img src="assets/img/services/epc-service.jpg" alt="" class="img-fluid">
+                    <!-- <img src="assets/img/logo/hexa-icon.png" alt="" class="top-hexa"> -->
+                    <img src="assets/img/logo/hexa-icon.svg" alt="" class="top-hexa">
+                </div>
+                <div class="service-desc">
+                    <h2 class="service-head">Engineering, Procurement & Construction</h2>
+                    <p class="service-para">Since its infrastructure has a major impact on cities, regions and their
+                        citizens, hexa E.P.C. aims for quality, technically sound, economically viable,
+                        environment-friendly and socially beneficial performance</p>
+                    <a href="" class="service-btn">
+                        <p>Explore</p>
+                        <img src="assets/img/icons/arrow.svg" alt="" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="img-div">
+                    <img src="assets/img/services/business-engineering-service.jpg" alt="" class="img-fluid">
+                    <!-- <img src="assets/img/logo/hexa-icon.png" alt="" class="top-hexa"> -->
+                    <img src="assets/img/logo/hexa-icon.svg" alt="" class="top-hexa">
+                </div>
+                <div class="service-desc">
+                    <h2 class="service-head">Business Intelligence</h2>
+                    <p class="service-para">GCC’s Leading Cloud ERP Software For Contracting, Trading And Manufacturing Companies, to simplify the Contracting or Trading or Manufacturing business with Tranquil ERP Software and manage entire business process </p>
+                    <a href="" class="service-btn">
+                        <p>Explore</p>
+                        <img src="assets/img/icons/arrow.png" alt="" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="img-div">
+                    <img src="assets/img/services/chemical-engineering-service.jpg" alt="" class="img-fluid">
+                    <!-- <img src="assets/img/logo/hexa-icon.png" alt="" class="top-hexa"> -->
+                    <img src="assets/img/logo/hexa-icon.svg" alt="" class="top-hexa">
+                </div>
+                <div class="service-desc">
+                    <h2 class="service-head">Chemicals Manufacturing & Distributions</h2>
+                    <p class="service-para">A new-generation Oil Field Chemical manufacturer & supplier offering a complete portfolio of chemicals right from to Drilling, Completion & Production. Our efforts are continuous on enhancing the quality of diversified substances to make the process more effective</p>
+                    <a href="" class="service-btn">
+                        <p>Explore</p>
+                        <img src="assets/img/icons/arrow.png" alt="" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="img-div">
+                    <img src="assets/img/services/it-service.jpg" alt="" class="img-fluid">
+                    <!-- <img src="assets/img/logo/hexa-icon.png" alt="" class="top-hexa"> -->
+                    <img src="assets/img/logo/hexa-icon.svg" alt="" class="top-hexa">
+                </div>
+                <div class="service-desc">
+                    <h2 class="service-head">IT Enable Service</h2>
+                    <p class="service-para">Hexa Business Transformation Services (Currently Biztras IT Solutions LLC) was founded with the core objective to provide public and private sectors across the globe with best-in-class IT services and Enterprise Business Solutions.</p>
+                    <a href="" class="service-btn">
+                        <p>Explore</p>
+                        <img src="assets/img/icons/arrow.png" alt="" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="img-div">
+                    <img src="assets/img/services/hr-solution.jpg" alt="" class="img-fluid">
+                    <!-- <img src="assets/img/logo/hexa-icon.png" alt="" class="top-hexa"> -->
+                    <img src="assets/img/logo/hexa-icon.svg" alt="" class="top-hexa">
+                </div>
+                <div class="service-desc">
+                    <h2 class="service-head">HR Solution</h2>
+                    <p class="service-para">Axxon is innovative & powerful workforce solutions. Headquartered in the capital city of Abu Dhabi, Axxon HR Solutions provides exceptional workforce solutions tailored to meet the needs of businesses</p>
+                    <a href="" class="service-btn">
+                        <p>Explore</p>
+                        <img src="assets/img/icons/arrow.png" alt="" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="img-div">
+                    <img src="assets/img/services/media.jpg" alt="" class="img-fluid">
+                    <!-- <img src="assets/img/logo/hexa-icon.png" alt="" class="top-hexa"> -->
+                    <img src="assets/img/logo/hexa-icon.svg" alt="" class="top-hexa">
+                </div>
+                <div class="service-desc">
+                    <h2 class="service-head">Media Service</h2>
+                    <p class="service-para">We shoot advertisement, we make corporate films, we make films, we are oneness. Oneness has a handpicked pool of talented artists, visualizer, strategists & writer, designers & developers and illustrators.  </p>
+                    <a href="" class="service-btn">
+                        <p>Explore</p>
+                        <img src="assets/img/icons/arrow.png" alt="" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+<?php include('inc/footer.php');?>
+
+
+
+
+
+
+
+
+
+    <!-- JQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap CDN SCRIPT -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script> -->
+
+    <!-- Counter -->
+    <script src="assets/js/jquery.waypoints.min.js"></script>
+    <script src="assets/js/jquery.countup.js"></script>
+
+    <!-- Font Awesome CDN -->
+    <script src="https://kit.fontawesome.com/275b00d5ce.js" crossorigin="anonymous"></script>
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+
+
+    <script>
+        $('.counter').countUp({
+            delay: 5,
+            time: 1000,
+        });
+    </script>
+
+</body>
+
+</html>
---
+++ contact.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hexa-Contact</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="assets/img/logo/favicon.png" type="image/x-icon">
+
+
+    <!-- Bootstrap CDN CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- owl carousel -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
+
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+
+
+
+    <!-- CONTACT SECTION -->
+    <div id="contact-section">
+
+        <?php include('inc/header.php'); ?>
+
+
+        <div class="inner-pg-bnner">
+            <div class="container">
+                <div class="inner-main-title">
+                    <h2>Contact Hexa</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <!-- FORM SECTION -->
+    <div id="form-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-6 left">
+                    <div class="img-div">
+                        <img src="assets/img/contact-img.png" alt="" class="img-fluid">
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 right">
+                    <div>
+                        <h2 class="main-title">Connect with <span class="main-grade">Hexa</span></h2>
+                        <p class="main-para text-start">Fill up the Form and our team will contact you.</p>
+                    </div>
+                    <div class="main-form-div">
+                        <form action="">
+                            <input type="text" placeholder="Enter Name">
+                            <input type="mail" placeholder="Enter Email">
+                            <input type="phone" placeholder="Enter Phone Number">
+                            <textarea name="" id="" cols="30" rows="10" placeholder="Comment Here"></textarea>
+                            <button class="main-btn">SUBMIT</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <?php include('inc/footer.php'); ?>
+
+
+
+    <!-- JQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap CDN SCRIPT -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script> -->
+
+    <!-- Counter -->
+    <script src="assets/js/jquery.waypoints.min.js"></script>
+    <script src="assets/js/jquery.countup.js"></script>
+
+    <!-- Font Awesome CDN -->
+    <script src="https://kit.fontawesome.com/275b00d5ce.js" crossorigin="anonymous"></script>
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+
+
+</body>
+
+</html>
